9ca33772a8b782d328cc280e06dc68841e4dc94c
sumraharshit <149246914+sumraharshit@users.noreply.github.com>
2023-10-29 19:33:33 +0530

    Update grid.cpp
    As the color.h file has been included which takes care of defining the colours of the block as well as of the grid, so it was removed from the grid.

--- grid.cpp
+++ grid.cpp
@@ -1,5 +1,6 @@
 #include "grid.h"
 #include <iostream>
+#include "color.h"
 using namespace std;
 
 Grid::Grid()
@@ -35,19 +36,6 @@ void Grid::Initialise()
    
  }
 // after we have printed the grid and gave it the value 0 we have to guve colors to different blocks of tetris
-std::vector<Color> Grid::GetCellColors()
-{
-   Color darkgrey={26, 31, 40, 255};
-   Color green={47, 230, 23, 255};
-   Color red={232, 18, 18, 255};
-   Color orange={226, 116, 17, 255};
-   Color yellow={237, 234, 4, 255};
-   Color purple={166, 0, 247, 255};
-   Color cyan={21, 204, 209, 255};
-   Color blue={13, 64, 216, 255};
-
-   return {darkgrey, green, red, orange, yellow, purple,cyan, blue};
-}
 
 void Grid::Draw()
 {
