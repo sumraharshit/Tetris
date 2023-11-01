d841207804bbd74dd40a424bc098531a0e694ed7
Harshit Sumra <149246914+sumraharshit@users.noreply.github.com>
2023-11-01 18:22:28 +0530

    Update main.cpp
    1. Added time intervals to drop the blocks automatically.
    2.  Handle Input Function Implemented

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <raylib.h>
-#include "grid.h"
-
+#include "game.h"
 double LastTimeUpdation =0;
 
 bool MovingBlockDown(double interval)
@@ -18,10 +17,9 @@ int main()
    InitWindow(300, 600, "Tetris") ; //initialises window and take 3 values
    SetTargetFPS(60);   //if not define the computer will run it as fast as it can
     //Defining The game loop
-    Grid grid = Grid();
-    grid.Print();
+    Game game=Game();
     while(WindowShouldClose()==false) //windowshouls=dclose checks whether the esc or quit icon is pressed or not, if pressed it returns true
-   {  game.HandleInput();
+   {   game.HandleInput();
      // THE FUNCTION IS DEFINED TO DROP THE BLOCK AS IN CLAASSIC TETRIS
     //NOW THE ISSUE IS THAT THE BLOCK DROPS EVERY QUICKLY THATS BEACAUSE WE HAVE SET THE FPS TO 60, MEANING THAT THE BLOCK DROPS 60
     //GRIDS EVERY SECOND, NOW TO FIX THAT WE CAN IMPLEMENT EVENT TRIGGER THAT WILL MAKE SURE THAT THE BLOCK DROPS AFTER A CERTAIN TIME
@@ -33,8 +31,8 @@ int main()
     }
      BeginDrawing(); //begin drawning canvas
      ClearBackground(darkblue);
-     // implenmneting draw in grid
-     grid.Draw();
+     // implenmneting draw in game which will draw with help of of grid and block
+       game.Draw();
      // now to make tetris more understandable we will make lines
      // for that we will use space of 1 pixel from each cell meaning 30(i.e cell size)-1 = 29 margin
      EndDrawing();   //ends drawing canvas
