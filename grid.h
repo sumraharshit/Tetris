5e3e0eaffc834522d80ecb2d0a3ad87594c3dc6f
Harshit Sumra <149246914+sumraharshit@users.noreply.github.com>
2023-11-01 18:25:00 +0530

    Update grid.h
    1. Function to check where the block is outside, declared.
    2. Function to clear row when full, declared.

--- grid.h
+++ grid.h
@@ -11,11 +11,17 @@ public:
     void Initialise(); // making it public as it is required by other parts of the program as well
     void Print(); 
     void Draw();
-   
+    bool isCellOutsideGrid(int row, int column);
+    bool CellEmpty(int row, int column);
+
+    int ClearFullRows(); // a implementation of all the Rowfull(), RowClear(), MoveRow();
+
 private:
    int numRows;
    int numColms;
    int cellSize;
-   // after we have printed the grid and gave it the value 0 we have to guve colors to different blocks of tetris
-   std::vector<Color> colours;
+   std::vector<Color> colours; // after we have printed the grid and gave it the value 0 we have to guve colors to different blocks of tetris
+   bool Rowfull(int row); //checks whether the row is full or not
+   void RowClear(int row); // clears the row when the row is full
+   void MoveRow(int row, int numRows); // Moving the rows down when cleared
 };
